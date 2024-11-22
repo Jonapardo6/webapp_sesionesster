26b5b1b0f36251d93cb0c2b8569bfa113e2c2f46
Prueba1 <jonapardo6@gmail.com>
2024-11-22 12:03:34 -0500

    gterminarrrrr

--- src/main/java/controllers/Logout.java
+++ src/main/java/controllers/Logout.java
@@ -1,4 +1,26 @@
 package controllers;
 
-public class Logout {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import services.LoginService;
+import services.LoginServiceImplement;
+
+import java.io.IOException;
+import java.util.Optional;
+@WebServlet ("/logout")
+public class Logout extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        LoginService auth= new LoginServiceImplement();
+        Optional<String> usernameOptional=auth.getUserName(req);
+        if(usernameOptional.isPresent()){
+            HttpSession session=req.getSession();
+            session.invalidate();
+        }
+        resp.sendRedirect(req.getContextPath()+"/login.html");
+    }
 }
